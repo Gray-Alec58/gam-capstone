3b38f6ee2ce7077ece952044af13886d58200677
CheezyWolf <bonnieni1234@gmail.com>
2026-09-17 16:10:09 -0400

    added most button states

--- Scenes/Scripts/full_screen_container.gd
+++ Scenes/Scripts/full_screen_container.gd
@@ -6,42 +6,6 @@ extends MarginContainer
 @export var ExpandWindow: MarginContainer
 @export var FundraiseWindow: MarginContainer
 
-func _on_research_app_pressed() -> void:
-	ResearchWindow1.visible = true
-	ResearchWindow2.visible = false
-	RescueWindow.visible = false
-	ExpandWindow.visible = false
-	FundraiseWindow.visible = false
-	
-func _on_farm_animals_pressed() -> void:
-	ResearchWindow1.visible = false
-	ResearchWindow2.visible = true
-	RescueWindow.visible = false
-	ExpandWindow.visible = false
-	FundraiseWindow.visible = false
-
-func _on_rescue_app_pressed() -> void:
-	ResearchWindow1.visible = false
-	ResearchWindow2.visible = false
-	RescueWindow.visible = true
-	ExpandWindow.visible = false
-	FundraiseWindow.visible = false
-	
-func _on_build_app_pressed() -> void:
-	ResearchWindow1.visible = false
-	ResearchWindow2.visible = false
-	RescueWindow.visible = false
-	ExpandWindow.visible = true
-	FundraiseWindow.visible = false
-
-func _on_fundraise_app_pressed() -> void:
-	ResearchWindow1.visible = false
-	ResearchWindow2.visible = false
-	RescueWindow.visible = false
-	ExpandWindow.visible = false
-	FundraiseWindow.visible = true
-
-
 func _on_research_texture_button_pressed() -> void:
 	ResearchWindow1.visible = true
 	ResearchWindow2.visible = false
@@ -69,3 +33,10 @@ func _on_fundraise_texture_button_pressed() -> void:
 	RescueWindow.visible = false
 	ExpandWindow.visible = false
 	FundraiseWindow.visible = true
+
+func _on_farm_animal_texture_button_pressed() -> void:
+	ResearchWindow1.visible = false
+	ResearchWindow2.visible = true
+	RescueWindow.visible = false
+	ExpandWindow.visible = false
+	FundraiseWindow.visible = false
