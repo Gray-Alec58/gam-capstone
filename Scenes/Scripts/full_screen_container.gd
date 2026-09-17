b3bfa1785e50abc09418f18f5e2d5f8270059e27
CheezyWolf <bonnieni1234@gmail.com>
2026-09-17 15:04:09 -0400

    added button states and made them work

--- Scenes/Scripts/full_screen_container.gd
+++ Scenes/Scripts/full_screen_container.gd
@@ -40,3 +40,32 @@ func _on_fundraise_app_pressed() -> void:
 	RescueWindow.visible = false
 	ExpandWindow.visible = false
 	FundraiseWindow.visible = true
+
+
+func _on_research_texture_button_pressed() -> void:
+	ResearchWindow1.visible = true
+	ResearchWindow2.visible = false
+	RescueWindow.visible = false
+	ExpandWindow.visible = false
+	FundraiseWindow.visible = false
+
+func _on_rescue_texture_button_pressed() -> void:
+	ResearchWindow1.visible = false
+	ResearchWindow2.visible = false
+	RescueWindow.visible = true
+	ExpandWindow.visible = false
+	FundraiseWindow.visible = false
+
+func _on_build_texture_button_pressed() -> void:
+	ResearchWindow1.visible = false
+	ResearchWindow2.visible = false
+	RescueWindow.visible = false
+	ExpandWindow.visible = true
+	FundraiseWindow.visible = false
+
+func _on_fundraise_texture_button_pressed() -> void:
+	ResearchWindow1.visible = false
+	ResearchWindow2.visible = false
+	RescueWindow.visible = false
+	ExpandWindow.visible = false
+	FundraiseWindow.visible = true
